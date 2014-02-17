3d72a288c2587761cc7014e6ef44c54a1ba16289
ada10lgu <ada10lgu@student.lth.se>
2014-02-17 15:57:47 +0100

    ARFF: Can now itterate over all rows

--- src/arff/Data.java
+++ src/arff/Data.java
@@ -1,29 +1,68 @@
 package arff;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
-public class Data {
+public class Data implements Iterable<String[]> {
 
 	private String relation;
 	private ArrayList<String> attribute;
- 	
+ 	private ArrayList<String[]> rows;
 	
 	Data(){
 		attribute = new ArrayList<String>();
+		rows = new ArrayList<String[]>();
 	}
 	
 	void setRelation(String relation) {
 		this.relation = relation;
-		
 	}
 	
 	void addAttribute(String a) {
 		attribute.add(a);
 	}
 	
+	public int getSize() {
+		return rows.size();
+	}
+
+	
+	
 	@Override
 	public String toString() {
-		return relation;
+		StringBuilder sb = new StringBuilder();
+		String nl = System.getProperty("line.separator");
+		sb.append(relation).append(nl);
+		
+		sb.append("Number of attributes: ").append(attribute.size()).append(nl);
+		
+		return sb.toString();
+	}
+
+	@Override
+	public Iterator<String[]> iterator() {
+		return new DataIterator();
+	}
+	
+	private class DataIterator implements Iterator<String[]> {
+
+		int i = 0;
+		
+		@Override
+		public boolean hasNext() {
+			return i<rows.size();
+		}
+
+		@Override
+		public String[] next() {
+			return rows.get(i++);
+		}
+
+		@Override
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+		
 	}
 	
 }
